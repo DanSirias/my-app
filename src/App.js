a8843ab5d81dd3bb6b2b5e04e6390dfa05e5c108
DanSirias <danielsirias.88@gmail.com>
2023-05-01 23:05:38 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -3,25 +3,135 @@
 //Component returns HTML must start with CAPS
 
 import './App.css';
+//import {User} from './User'; 
+import { useState } from "react"; 
+
+
+//Ternary Operator 
+//age >= 18 ? console.log("HEEH") : console.log("NO") // (? is if) (: is else) (&& if something is true)
+
+//States
+// Is a variable that holds data (string, numeric, boleen, )
+
+
 
 function App() {
-  const name =  <h1>Daniel</h1>; 
-  const age = <h2> 34</h2>; 
-  const email = <h3> daniel.sirias@outlook.com</h3>
-  const user = (
-    <div>{name}{age}{email}</div>
-  );
 
-  return (<div className="App">Howdy World
-    <User />
-  </div>
-  );
+const [textColor, setShowText] = useState("black");
+
+const toggleText = () => {
+
 }
 
-const User = (props) => {
+  return (
+    <div className="App">
+      <>
+      <button class='btn-success' onMouseOver={()=>{setShowText(textColor === "black" ? "blue" : "black")}}> Show/Hide</button>
+      <h1 style={{color: textColor}}>Howdy, my name is Daniel</h1>
+      </>
+    </div>
+  )
 
-  return (<div><h1>Daniel</h1><h2> 34</h2><h3> daniel.sirias@outlook.com</h3></div>); 
-  
-};
+}
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Props
+
+//<User name = "Becca" age={35} email="rbeccas"/>
+//<Job salary = {90000} position="Senior Dev" company="Amazon"/>
+
+/* const Job = (props) => {
+  return (<div>
+    <p>{props.salary}</p>
+    <p>{props.position}</p>
+    <p>{props.company}</p>
+  </div>)
+};
+
+
+const User = (props) => {
+  return (<div>
+    <h1>{props.name}</h1>
+    <h2>{props.age}</h2>
+    <h3>{props.email}</h3></div>); 
+};
+
+Conditions: 
+      {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+      <h1>This has color</h1>
+      {isGreen && <button className="btn-success">This is a Button</button>}
+
+Arrays: 
+
+  const names = ["Dan", "Sam", "Dina"]; 
+  const users =[
+    {name: "Timmy", age: 3}, 
+    {name: "Sammy", age: 7}, 
+    {name: "Sally", age: 10} 
+  ]
+
+   {names.map((name, idx) => {
+        return <h1 key={idx}>{name} hello</h1>
+      })}
+
+loops: 
+
+  const planets = [
+    { name: 'Mars', isGasPlanet: false }, 
+    { name: 'Earth', isGasPlanet: false }, 
+    { name: 'Jupiter', isGasPlanet: true }, 
+    { name: 'Venus', isGasPlanet: false }, 
+    { name: 'Neptune', isGasPlanet: true }, 
+    { name: 'Uranus', isGasPlanet: true }
+  ]; 
+
+  return (
+    <div className="App">
+      {planets.map((planet, key) => planet.isGasPlanet && <h1>{planet.name}</h1>)}
+    </div>
+  )
+
+States: 
+import { useState } from "react"; 
+const [age, setAge] = useState(3); 
+const IncreaseAge = () => {
+  setAge(age + 1); 
+}
+
+const [text, setText] = useState('') 
+const handleInputChange = (e)=>{
+  setText(e.target.value);
+};
+      <input type='text' onChange={handleInputChange}/>
+      <span>{text}</span>
+
+
+const [textColor, setShowText] = useState("black");
+
+<button class='btn-success' onMouseOver={()=>{setShowText(textColor === "black" ? "blue" : "black")}}> Show/Hide</button>
+<h1 style={{color: textColor}}>Howdy, my name is Daniel</h1>
+
+
+*/
